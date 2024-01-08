fd50cefd038ebdca5f6aef283e4b1e425a434098
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-08 06:18:36 +0200

    Update 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    newString = ""
+    for cha in my_string:
+        if cha is not 'c' and cha is not 'C':
+          newString = newString + cha
+    return newString
