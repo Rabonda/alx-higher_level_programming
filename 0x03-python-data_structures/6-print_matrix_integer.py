094d09dbe04dbb137ccf03954bf189096a441c9d
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-08 06:20:25 +0200

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    newString = ""
-    for cha in my_string:
-        if cha is not 'c' and cha is not 'C':
-          newString = newString + cha
-    return newString
+def print_matrix_integer(matrix=[[]]):
+    for num in matrix:
+        print(' '.join("{:d}".format(cha) for cha in num))
