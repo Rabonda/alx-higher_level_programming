86695b0837bf18bf591a491721a3e94ccfb31307
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-08 11:13:12 +0200

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for elements in my_list:
-        print("{:d}".format(elements), end="\n")
+    for list_elements in my_list:
+        print("{:d}".format(list_elements), end="\n")
