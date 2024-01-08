e7eaa1223157a416b89d011ba644c10bd3abc72b
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-08 08:17:05 +0200

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def max_integer(my_list = []):
-  list_size = len(my_list)
-  if list_size == 0:
-    return
-  my_list.sort()
-  return (my_list.pop())
+def max_integer(my_list=[]):
+        return (min(my_list, key=lambda x: -x)) if my_list else None
