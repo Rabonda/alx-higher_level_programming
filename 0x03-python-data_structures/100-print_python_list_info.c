d7ebae7f7f24466226335cbd0837b4c2c408b9db
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-08 10:34:45 +0200

    Update 100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,16 +1,18 @@
 #include <Python.h>
+
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t ssizeP, alloc;
+	Py_ssize_t size_p, allocated, idx = 0;
 	PyObject *element;
 
-	ssizeP = PyList_Size(p);
-	alloc = ((PyListObject *)p)->allocated;
-	printf("[*] Size of the Python List = %ld\n", ssizeP);
-	printf("[*] Allocated = %ld\n", alloc);
-	for (Py_ssize_t x = 0; x < ssizeP; x++)
+	size_p = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %ld\n", size_p);
+	printf("[*] Allocated = %ld\n", allocated);
+	while (idx < size_p)
 	{
 		element = PyList_GET_ITEM(p, idx);
-		printf("Element %ld: %s\n", x, element->ob_type->tp_name);
+		printf("Element %ld: %s\n", idx, element->ob_type->tp_name);
+		idx++;
 	}
 }
