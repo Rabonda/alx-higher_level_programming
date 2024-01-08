3d44182e03a16d99bdf42052bc503e3c477eca62
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-08 06:11:02 +0200

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    newString = " "
+    newString = ""
     for cha in my_string:
         if cha is not 'c' and cha is not 'C':
             newString = newString + cha
