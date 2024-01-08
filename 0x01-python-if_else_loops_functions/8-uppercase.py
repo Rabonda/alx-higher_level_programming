34b1d5d473870326feadd1b090352c199c69360c
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-08 22:50:13 +0200

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@ def uppercase(str):
     for alpha in str:
         char = ord(alpha)
         if ord(alpha) <= 96 or ord(alpha) >= 123:
-          pass
+            pass
         else:
-          char = ord(alpha) - 32
+            char = ord(alpha) - 32
         print("{:c}".format(char), end="")
     print()
