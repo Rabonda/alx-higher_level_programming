61331b3dceaf3c6a1bc80b485894dab72d4fc93f
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-08 14:55:53 +0200

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    print("{:s}".format(chr(char)), end='')
+for alpha in range(ord('a'), ord('z') + 1):
+    print("{:s}".format(chr(alpha)), end="")
