d80b47131e2391426e4e2989d194d1bdadf7ffdb
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-08 06:03:08 +0200

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    newString = ""
-    for num in my_string:
-        if num is not 'c' and num is not 'C':
-            newString = newString + num
-    return newString
+    new = ""
+    for i in my_string:
+        if i is not 'c' and i is not 'C':
+            new += i
+    return new
