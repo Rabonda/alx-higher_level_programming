c09c3cfe01e381a01732de5e18c79c01f47f7f06
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-08 08:28:18 +0200

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-  for x in my_list:
-    if x % 2 == 0:
-      list = True
-    else:
-      list = False
+  list = [True if num % 2 == 0 else False for num in my_list]
   return list
