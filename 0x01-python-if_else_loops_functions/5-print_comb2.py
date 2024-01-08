567de4616a7b15ac556f2e3ee6a6732fb1cc87db
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-08 22:05:31 +0200

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for indx in range(0,99):
-  print('{:02d}'.format(indx), end=", ")
+for indx in range(100):
+    if indx != 99:
+        print('{:02d}'.format(indx), end=", ")
 print(indx)
