dc72342df1ff3f56ee4d4e6e46944824ec5ddac7
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-09 10:03:43 +0200

    13-insert_number.c

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * insert_node - inserts a number into a list
+ * @head: pointer to the list
+ * @number: number to sum
+ * Return: address
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *old_list, *new_list, *current_list;
+
+	if (!head)
+		return (NULL);
+
+	new_list = malloc(sizeof(listint_t));
+	if (!new_list)
+		return (NULL);
+
+	new_list->n = number;
+	old_list = NULL;
+	current_list = *head;
+
+	while (current_list != NULL && current_list->n < number)
+	{
+		old_list = current_list;
+		current_list = current_list->next;
+	}
+
+	new_list->next = current_list;
+
+	if (old_list != NULL)
+		old_list->next = new_list;
+	else
+		*head = new_list;
+
+	return (new_list);
+}
