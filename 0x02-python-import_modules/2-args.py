e74dc7dcea27ad5bb833a8dd62543fe6be37f1fd
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-11 12:58:55 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     args = len(sys.argv) - 1
-    if args is 0:
+    if args == 0:
         print("{:d} arguments.".format(args))
     elif args is 1:
         print("{:d} argument:".format(args))
