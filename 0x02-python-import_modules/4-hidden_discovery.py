cb2be4d2bb07b5c7f31e5aa0b6d24fdff37c915e
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-11 17:03:16 +0200

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.4
+def hidden_discovery():
+    hide = dir(hidden_4)
+    for indx in hide:
+        if(indx[:2] != "__"):
+            print(indx)
+
+if __name__ == "__main__":
+    import hidden_4
+    hidden_discovery()
