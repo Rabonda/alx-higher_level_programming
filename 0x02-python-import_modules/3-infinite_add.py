60e9d5997922f8d31b37f40d5ff4e637bd5e1e07
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-11 13:10:31 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+def add():
     total = 0
     for indx in range(1, len(sys.argv)):
         total = total + int(sys.argv[indx])
-    print (total)
+    print(total)
+
+if __name__ == "__main__":
+    import sys
+    add()
