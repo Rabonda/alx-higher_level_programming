5b8335c19f196d55f98801a388706c207add8dd6
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-11 18:18:37 +0200

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-    
+
+
 def calculator(args):
     if len(args) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -19,7 +20,7 @@ def calculator(args):
     if args[1] == "/":
         print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
 
+
 if __name__ == "__main__":
     import sys
     calculator(sys.argv[1:])
-    
