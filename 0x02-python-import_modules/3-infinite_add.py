23779a9ef7e9d8b7a64fd3ee7547f53a8fe03809
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-11 13:06:02 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-def total(args):
-    tot = 0
-    for lists in args[1:]:
-        tot = tot + int(lists)
-    print tot
-
 if __name__ == "__main__":
     import sys
-    total(sys.argv)
+    total = 0
+    for indx in range(1, len(sys.argv)):
+        total = total + int(sys.argv[indx])
+    print total
