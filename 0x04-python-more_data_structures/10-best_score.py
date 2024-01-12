fb4cac939f14feceb2d6a71810e389f1159c1abf
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-12 10:54:39 +0200

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,4 @@ def best_score(a_dictionary):
     if a_dictionary:
         dictionary_2 = sorted((value, key)
         for (key, value) in a_dictionary.items()).pop()[1]
-            return (dictionary_2)
+        return (dictionary_2)
