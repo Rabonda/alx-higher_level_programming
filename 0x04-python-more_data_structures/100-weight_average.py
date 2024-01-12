5d472d8d42b7ebfb17db7ea39d74406f02bcd6a5
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-12 11:33:34 +0200

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if !my_list:
+    if not my_list:
         return 0
-    ave = 0
-    divide = 0
+    average = 0
+    div = 0
     for tup in my_list:
-        ave += tup[0] * tup[1]
-        divide += tup[1]
-    return float(ave / divide)
+        average += tup[0] * tup[1]
+        div += tup[1]
+    return float(average / div)
