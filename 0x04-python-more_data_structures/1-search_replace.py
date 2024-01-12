f22eaaa4258c420b043676100ab665c055fc6e97
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-12 08:54:54 +0200

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 def search_replace(my_list, search, replace):
     list_2 = []
     if my_list:
-        list_2 = list(map(lambda l: l if l != search else replace, my_list))
+        list_2 = list(map(lambda x: x if x != search else replace, my_list))
     return list_2
