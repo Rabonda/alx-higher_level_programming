879bc00872bb62ebfb7f330499ad44d2e2ab23a1
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-22 13:47:38 +0200

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    indx = 0
+    while indx < x:
+        try:
+            print("{:d}".format(my_list[num]), end='')
+            indx = indx + 1
+        except IndexError:
+            pass
+    print()
+    return indx
