0bb4c440c022e5b9450dc7719421e23a920c8fc6
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-22 13:18:05 +0200

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    ind = 0
+
     try:
-        for ind = 0; ind < x; ind++:
+        for ; ind < x; ind++:
             print("{:d}".format(my_list[ind]), end="")
     except:
         pass
