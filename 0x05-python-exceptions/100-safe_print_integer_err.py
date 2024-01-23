ab9ce067cc842378fe05e7c8f2724cf63bc2929e
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-23 09:15:13 +0200

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import sys
-
 def safe_print_integer_err(value):
+    from sys import stderr
     try:
         print("{:d}".format(value))
         return True
     except Exception as err:
-          print("Exception:", err, file=sys.stderr)
+        print("Exception: {}".format(err), file=stderr)
         return False
