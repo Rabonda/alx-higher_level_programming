7f615bc873519239562c4520b183fab79a93af20
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-23 10:18:11 +0200

    102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    r = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            r = r + a ** b / i
+        except:
+            r = a + b
+            break
+    return r
