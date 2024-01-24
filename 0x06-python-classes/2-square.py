2f19d9182475ca8a3e45121114b7c71e4f15facb
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-24 14:31:52 +0200

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,10 @@
 
 
 class Square:
-    """Represents a square""" 
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with optional size.
+    """ 
     def __init__(self, size=0):
         """Initializes the __size variable with the size value."""
         if size < 0:
