280fdb8f36682b265517599e0d7c9d7605802175
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-24 14:34:29 +0200

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,8 +9,8 @@ class Square:
     """ 
     def __init__(self, size=0):
         """Initializes the __size variable with the size value."""
-        if size < 0:
-            raise ValueError("size must be >= 0") 
-        elif not isinstance(size, int):
-            raise TypeError("size must be an integer") 
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
