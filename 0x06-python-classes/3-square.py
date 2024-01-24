801da3b90bf67dc849913d3742bd47b193226776
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-24 14:51:50 +0200

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """Definition of a class named Square"""
+
+    def __init__(self, size=0):
+        """initialization"""
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """area of a square method"""
+        area = self.__size**2
+        return area
