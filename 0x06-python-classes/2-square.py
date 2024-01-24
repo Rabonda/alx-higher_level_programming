00981cc8aecaba8bbbfb3f15208b50d5ff190207
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-24 14:28:27 +0200

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Definition of a class, named Square"""
+
+
+class Square:
+
+    def __init__(self, size=0):
+        """Initializes the __size variable with the size value."""
+        if size < 0:
+            raise ValueError("size must be >= 0") 
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer") 
+        self.__size = size
