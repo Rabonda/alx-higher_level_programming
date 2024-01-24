93e5d94f099ce3287e3b27c793f8e9577a1d9a55
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-24 14:57:12 +0200

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""
+Definition of a class, named Square
+"""
 
 
 class Square:
     """
-    Class that defines properties of square by: (based on 2-square.py).
-
-    Attributes:
-        size: size of a square (1 side).
+    Initialization method
     """
     def __init__(self, size=0):
         """Creates new instances of square.
-
-        Args:
-            size: size of the square (1 side).
         """
         self.__size = size
 
