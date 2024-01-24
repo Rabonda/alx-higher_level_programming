37a8c42d55ded862f8f9b162b387a9e6d595191a
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-24 15:42:52 +0200

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Definition of a class, named Square"""
+
+
+class Square:
+    """
+    Class that defines properties of square. 
+    """
+    def __init__(self, size=0):
+        """Creates new instances of square. 
+        """
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of square.
+        """
+        a = self.__size ** 2
+        return a
+
+    @property
+    def size(self):
+        """Returns the size of a square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property setter for size. 
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
