cc2dae8de30debeb3e107f58a23108940571eedc
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-24 16:25:15 +0200

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,15 +4,23 @@
 
 class Square:
     """
-    Initializes a class
+    Class that defines properties of square by: (based on 4-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
     """
     def __init__(self, size=0):
         """Creates new instances of square.
+
+        Args:
+            size: size of the square (1 side).
         """
         self.__size = size
 
     def area(self):
         """Calculates the area of square.
+
+        Returns: the current square area.
         """
         return self.__size ** 2
 
@@ -24,7 +32,14 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """Property setter for size. 
+        """Property setter for size.
+
+        Args:
+            value (int): size of a square (1 side).
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -34,7 +49,7 @@ class Square:
             self.__size = value
 
     def my_print(self):
-        """prints in stdout
+        """prints in stdout the square with the character #
         """
 
         if self.__size == 0:
